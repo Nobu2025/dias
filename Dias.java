dbd583359e448e455cbe0a12131658badbb01813
Nobu2025 <20243rd038@utez.edu.mx>
2025-05-13 11:45:41 -0600

    se crearon los dias de la semana

---
+++ Dias.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Dias{
+    public static void main(String[] args){
+        Scanner numero1 = new Scanner(System.in);
+        System.out.println("Escribe un numero");
+
+        if(numero1 == 1){
+            System.out.println("hoy es lunes");
+        }else if(numero1 == 2){
+            System.out.println("hoy es martes");
+
+        }else if(numero1 == 3){
+            System.out.println("hoy es miercoles");
+
+        }else if(numero1 == 4){
+            System.out.println("hoy es jueves");
+
+        }else if(numero1 == 5){
+            System.out.println("hoy es viernes");
+
+        }else if(numero1 == 6){
+            System.out.println("hoy es sabado");
+
+        }else if(numero1 == 7){
+            System.out.println("hoy es domingo");
+
+        }
+    }
+   
+            
+            
+            
+    }
